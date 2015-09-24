9f351fb97e2e0e2696a6d397d54479fe0b17420a
Dan Thach <danthach6@gmail.com>
2015-09-23 18:11:30 -0600

    troubleshoot IE4

--- public/serverList/serverListCtrl.js
+++ public/serverList/serverListCtrl.js
@@ -796,27 +796,27 @@ angular.module('ServerList', ['SharedHTTP'])
   //     }
   // })
 
-  // .filter('secondsToTimeString', function() {
-  //     //Returns duration from milliseconds in hh:mm:ss format.
-  //     return function(seconds) {
-  //       var h = 3600;
-  //       var m = 60;
-  //       var hours = Math.floor(seconds/h);
-  //       var minutes = Math.floor( (seconds % h)/m );
-  //       var scnds = Math.floor( (seconds % m) );
-  //       var timeString = '';
-  //       if(scnds < 10) scnds = "0"+scnds;
-  //       if(hours < 10) hours = "0"+hours;
-  //       if(minutes < 10) minutes = "0"+minutes;
-  //       if (hours == "00") {
-  //         timeString = minutes +" minutes "+scnds +" seconds";
-  //       } else {
-  //         timeString = hours +"h  "+ minutes +"m  "+scnds +"s";
-  //       }
+  .filter('secondsToTimeString', function() {
+      //Returns duration from milliseconds in hh:mm:ss format.
+      return function(seconds) {
+        var h = 3600;
+        var m = 60;
+        var hours = Math.floor(seconds/h);
+        var minutes = Math.floor( (seconds % h)/m );
+        var scnds = Math.floor( (seconds % m) );
+        var timeString = '';
+        if(scnds < 10) scnds = "0"+scnds;
+        if(hours < 10) hours = "0"+hours;
+        if(minutes < 10) minutes = "0"+minutes;
+        if (hours == "00") {
+          timeString = minutes +" minutes "+scnds +" seconds";
+        } else {
+          timeString = hours +"h  "+ minutes +"m  "+scnds +"s";
+        }
 
-  //       return timeString;
-  //     }
-  // })
+        return timeString;
+      }
+  })
 
   // .filter('dateString', function($filter) {
   //   return function(date) {
